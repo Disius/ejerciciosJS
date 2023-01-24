7b6965f8dd2827d2f510b9bbab5c2d6e6a5d033b
Disius <defp_99@hotmail.com>
2023-01-24 15:30:27 -0600

    first commit

---
+++ my-data.js
@@ -0,0 +1,13 @@
+const myInformation = [
+    "Daniel Eduardo Fernández Pérez",
+    23,
+    true,
+    new Date(1999, 7, 24),
+    myFavoriteBook = {
+        title: "1984",
+        author: "George Orwell",
+        date: "08-06-1949"
+    }
+];
+
+console.log(myInformation)
